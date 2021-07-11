4b8c931797099f289f9bd89360179e5741e88bf7
Hasan-Theyab <hhdoop70@gmail.com>
2021-07-11 18:47:01 +0300

    finished city5:Lima

--- js/app.js
+++ js/app.js
@@ -258,3 +258,67 @@ let tokyo={
   paris.avgCookies()
   paris.render()
   paris.sumCookies()
+
+
+  let lima={
+    name: "Lima",
+    min : 2,
+    max: 16,
+    avg: 4.6,
+    randomCustomersArray:[],
+    avgCookiesperHour:[],
+    sumarr:[], 
+    hours :['6am: ','7am: ','8am: ','9am: ',
+      '10am: ','11am: '
+      ,'12pm: ','1pm: ','2pm: ',
+      '3pm: ','4pm: ','5pm: ',
+      '6pm: ','7pm: '], 
+    randomCustomers:function random(min, max) 
+    {        return Math.floor(Math.random() * (max - min + 1) ) + min; 
+    }
+    ,
+    randomCust: function (random) 
+    {          
+      for(let i=0;i<this.hours.length;i++)  
+      {             
+        this.randomCustomersArray.push(this.randomCustomers(this.min,this.max))       
+      }              }           
+    ,avgCookies: function (avg)    
+    {         
+      for(let i=0;i<this.randomCustomersArray.length;i++)  
+      {            
+        this.avgCookiesperHour.push(Math.floor(this.randomCustomersArray[i]*this.avg))   
+      }        
+    },     
+                                               
+    sumCookies: function ()     
+    {       
+      let sum=0       
+      for(let j=0;j<this.avgCookiesperHour.length;j++)    
+      {             
+        sum=sum+this.avgCookiesperHour[j]           
+        this.sumarr.push(sum)       
+      }              return sum;    
+    } 
+    , render: function () 
+    {
+      let p_element=document.createElement('p')
+      value3.appendChild(p_element)
+      p_element.textContent="Lima"
+      let ulElement=document.createElement('ul')
+      value3.appendChild(ulElement)
+      for(let i=0;i<this.hours.length;i++)
+      {  
+        let liElement=document.createElement('li')  
+        ulElement.appendChild(liElement)  
+        liElement.textContent=this.hours[i]+this.avgCookiesperHour[i]  + ' cookies' 
+      }    
+      let lastElement=document.createElement('li')
+      ulElement.appendChild(lastElement)
+      lastElement.textContent=`Total: ${this.sumCookies()} cookies`
+    }
+  }
+  lima.randomCust()
+  lima.avgCookies()
+  lima.render()
+  lima.sumCookies()
