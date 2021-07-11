cdf2c901b63c8bdce0d2541cbe895e93e62371b9
Hasan-Theyab <hhdoop70@gmail.com>
2021-07-11 18:43:32 +0300

    finished city3:Dubai

--- js/app.js
+++ js/app.js
@@ -132,4 +132,68 @@ let tokyo={
   tokyo.sumCookies()
 
 
+  let dubai={
+    name: "Dubai",
+    min : 11,
+    max: 38,
+    avg: 3.7,
+    randomCustomersArray:[],
+    avgCookiesperHour:[],
+    sumarr:[], 
+    hours :['6am: ','7am: ','8am: ','9am: ',
+      '10am: ','11am: '
+      ,'12pm: ','1pm: ','2pm: ',
+      '3pm: ','4pm: ','5pm: ',
+      '6pm: ','7pm: '], 
+    randomCustomers:function random(min, max) 
+    {        return Math.floor(Math.random() * (max - min + 1) ) + min; 
+    }
+    ,
+    randomCust: function (random) 
+    {          
+      for(let i=0;i<this.hours.length;i++)  
+      {             
+        this.randomCustomersArray.push(this.randomCustomers(this.min,this.max))       
+      }              }           
+    ,avgCookies: function (avg)    
+    {         
+      for(let i=0;i<this.randomCustomersArray.length;i++)  
+      {            
+        this.avgCookiesperHour.push(Math.floor(this.randomCustomersArray[i]*this.avg))   
+      }        
+    },     
+                                               
+    sumCookies: function ()     
+    {       
+      let sum=0       
+      for(let j=0;j<this.avgCookiesperHour.length;j++)    
+      {             
+        sum=sum+this.avgCookiesperHour[j]           
+        this.sumarr.push(sum)       
+      }              return sum;    
+    } 
+    , render: function () 
+    {
+      let p_element=document.createElement('p')
+      value2.appendChild(p_element)
+      p_element.textContent="Dubai"
+      let ulElement=document.createElement('ul')
+      value2.appendChild(ulElement)
+      for(let i=0;i<this.hours.length;i++)
+      {  
+        let liElement=document.createElement('li')  
+        ulElement.appendChild(liElement)  
+        liElement.textContent=this.hours[i]+this.avgCookiesperHour[i]  + ' cookies' 
+      }    
+      let lastElement=document.createElement('li')
+      ulElement.appendChild(lastElement)
+      lastElement.textContent=`Total: ${this.sumCookies()} cookies`
+    }
+  }
+  dubai.randomCust()
+  dubai.avgCookies()
+  dubai.render()
+  dubai.sumCookies()
+
+
   
